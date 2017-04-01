8251baa57965ac62e22d03567c796996a50109c8
engstk <eng.stk@sapo.pt>
2017-04-01 16:16:51 +0100

    drivers: wakeup: remove display hook overhead
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -16,7 +16,6 @@
 #include <linux/debugfs.h>
 #include <linux/types.h>
 #include <linux/moduleparam.h>
-#include <linux/display_state.h>
 #include <trace/events/power.h>
 
 #include "power.h"
@@ -782,10 +781,6 @@ void pm_print_active_wakeup_sources(void)
 	struct wakeup_source *ws;
 	int active = 0;
 	struct wakeup_source *last_activity_ws = NULL;
-
-	// kinda pointless to force this routine during screen on
-	if (is_display_on())
-		return;
 
 	rcu_read_lock();
 	list_for_each_entry_rcu(ws, &wakeup_sources, entry) {
