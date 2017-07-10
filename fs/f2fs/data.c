290e17839d93427680ddb62df39967dd7ca9338d
engstk <eng.stk@sapo.pt>
2017-07-10 13:05:30 +0100

    f2fs: catch up with linux-3.18.y backports
    squash merge up to f2fs, block_dump: give WRITE direction to submit_bio
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -225,7 +225,7 @@ submit_io:
 		trace_f2fs_submit_read_bio(sbi->sb, type, bio);
 	else
 		trace_f2fs_submit_write_bio(sbi->sb, type, bio);
-	submit_bio(0, bio);
+	submit_bio(bio_op(bio), bio);
 }
 
 static void __submit_merged_bio(struct f2fs_bio_info *io)
@@ -2130,7 +2130,7 @@ static sector_t f2fs_bmap(struct address_space *mapping, sector_t block)
 	return generic_block_bmap(mapping, block, get_data_block_bmap);
 }
 
-#ifdef CONFIG_F2FS_MIGRATION
+#ifdef CONFIG_MIGRATION
 #include <linux/migrate.h>
 
 int f2fs_migrate_page(struct address_space *mapping,
@@ -2194,7 +2194,7 @@ const struct address_space_operations f2fs_dblock_aops = {
 	.releasepage	= f2fs_release_page,
 	.direct_IO	= f2fs_direct_IO,
 	.bmap		= f2fs_bmap,
-#ifdef CONFIG_F2FS_MIGRATION
+#ifdef CONFIG_MIGRATION
 	.migratepage    = f2fs_migrate_page,
 #endif
 };
